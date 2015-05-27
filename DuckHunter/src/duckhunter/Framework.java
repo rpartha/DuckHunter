eef9eabb2217918f0c95f3f392c89bca03b2281a
Ramaseshan Parthasarathy <tilluboy@gmail.com>
2015-05-27 18:53:26 -0400

    hunter

---
+++ DuckHunter/src/duckhunter/Framework.java
@@ -0,0 +1,270 @@
+package duckhunter;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.*;
+import javax.imageio.ImageIO;
+
+public class Framework extends Canvas {
+    
+   
+    public static int frameWidth, frameHeight;
+    public static final long secInNanosec = 1000000000L, milisecInNanosec = 1000000L;
+    private final int GAME_FPS = 60;
+    private final long GAME_UPDATE_PERIOD = secInNanosec / GAME_FPS;
+    public static enum gameState{STARTING, VISUALIZING, GAME_CONTENT_LOADING, MAIN_MENU, OPTIONS, PLAYING, GAMEOVER, DESTROYED}
+    public static gameState gs;
+    private long gameTime, lastTime;
+    private Shooter game;
+    
+    
+    /**
+     * Image for menu.
+     */
+    private BufferedImage shootTheDuckMenuImg;    
+    
+    public Framework ()
+    {
+        super();
+        
+        gs = gs.VISUALIZING;
+        
+        //We start game in new thread.
+        Thread gameThread = new Thread() {
+            @Override
+            public void run(){
+                GameLoop();
+            }
+        };
+        gameThread.start();
+    }
+    
+    
+   /**
+     * Set variables and objects.
+     * This method is intended to set the variables and objects for this class, variables and objects for the actual game can be set in Game.java.
+     */
+    private void init()
+    {
+
+    }
+    
+    /**
+     * Load files - images, sounds, ...
+     * This method is intended to load files for this class, files for the actual game can be loaded in Game.java.
+     */
+    private void LoadContent()
+    {
+        try
+        {
+            URL shootTheDuckMenuImgURL = this.getClass().getResource("/shoot_the_duck/resources/images/menu.jpg");
+            shootTheDuckMenuImg = ImageIO.read(shootTheDuckMenuImgURL);
+        }
+        catch (IOException ex) {
+            Logger.getLogger(Framework.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * In specific intervals of time (GAME_UPDATE_PERIOD) the game/logic is updated and then the game is drawn on the screen.
+     */
+    private void GameLoop()
+    {
+        // This two variables are used in VISUALIZING state of the game. We used them to wait some time so that we get correct frame/window resolution.
+        long visualizingTime = 0, lastVisualizingTime = System.nanoTime();
+        
+        // This variables are used for calculating the time that defines for how long we should put threat to sleep to meet the GAME_FPS.
+        long beginTime, timeTaken, timeLeft;
+        
+        while(true)
+        {
+            beginTime = System.nanoTime();
+            
+            switch (gs)
+            {
+                case PLAYING:
+                    gameTime += System.nanoTime() - lastTime;
+                    
+                    game.update(gameTime, mousePosition());
+                    
+                    lastTime = System.nanoTime();
+                break;
+                case GAMEOVER:
+                    //...
+                break;
+                case MAIN_MENU:
+                    //...
+                break;
+                case OPTIONS:
+                    //...
+                break;
+                case GAME_CONTENT_LOADING:
+                    //...
+                break;
+                case STARTING:
+                    // Sets variables and objects.
+                    init();
+                    // Load files - images, sounds, ...
+                    LoadContent();
+
+                    // When all things that are called above finished, we change game status to main menu.
+                    gs = gameState.MAIN_MENU;
+                break;
+                case VISUALIZING:
+                    if(this.getWidth() > 1 && visualizingTime > secInNanosec)
+                    {
+                        frameWidth = this.getWidth();
+                        frameHeight = this.getHeight();
+
+                        // When we get size of frame we change status.
+                        gs = gameState.STARTING;
+                    }
+                    else
+                    {
+                        visualizingTime += System.nanoTime() - lastVisualizingTime;
+                        lastVisualizingTime = System.nanoTime();
+                    }
+                break;
+            }
+            
+            // Repaint the screen.
+            repaint();
+            
+            // Here we calculate the time that defines for how long we should put threat to sleep to meet the GAME_FPS.
+            timeTaken = System.nanoTime() - beginTime;
+            timeLeft = (GAME_UPDATE_PERIOD - timeTaken) / milisecInNanosec; // In milliseconds
+            // If the time is less than 10 milliseconds, then we will put thread to sleep for 10 millisecond so that some other thread can do some work.
+            if (timeLeft < 10) 
+                timeLeft = 10; //set a minimum
+            try {
+                 //Provides the necessary delay and also yields control so that other thread can do work.
+                 Thread.sleep(timeLeft);
+            } catch (InterruptedException ex) { }
+        }
+    }
+    
+    /**
+     * Draw the game to the screen. It is called through repaint() method in GameLoop() method.
+     */
+    @Override
+    public void paint(Graphics2D g2d)
+    {
+        switch (gs)
+        {
+            case PLAYING:
+                game.paint(g2d, mousePosition());
+            break;
+            case GAMEOVER:
+                game.gameIsOver(g2d, mousePosition());
+            break;
+            case MAIN_MENU:
+                g2d.drawImage(shootTheDuckMenuImg, 0, 0, frameWidth, frameHeight, null);
+                g2d.drawString("Use left mouse button to shot the duck.", frameWidth / 2 - 83, (int)(frameHeight * 0.65));   
+                g2d.drawString("Click with left mouse button to start the game.", frameWidth / 2 - 100, (int)(frameHeight * 0.67));                
+                g2d.drawString("Press ESC any time to exit the game.", frameWidth / 2 - 75, (int)(frameHeight * 0.70));
+                g2d.setColor(Color.white);
+                g2d.drawString("WWW.GAMETUTORIAL.NET", 7, frameHeight - 5);
+            break;
+            case OPTIONS:
+            break;
+            case GAME_CONTENT_LOADING:
+                g2d.setColor(Color.white);
+                g2d.drawString("GAME is LOADING", frameWidth / 2 - 50, frameHeight / 2);
+            break;
+        }
+    }
+    
+    /**
+     * Starts new game.
+     */
+    private void newGame()
+    {
+        // We set gameTime to zero and lastTime to current time for later calculations.
+        gameTime = 0;
+        lastTime = System.nanoTime();
+        
+        game = new Shooter();
+    }
+    
+    /**
+     *  Restart game - reset game time and call RestartGame() method of game object so that reset some variables.
+     */
+    private void restartGame()
+    {
+        // We set gameTime to zero and lastTime to current time for later calculations.
+        gameTime = 0;
+        lastTime = System.nanoTime();
+        
+        game.restart();
+        
+        // We change game status so that the game can start.
+        gs = gameState.PLAYING;
+    }
+    
+    /**
+     * Returns the position of the mouse pointer in game frame/window.
+     * If mouse position is null than this method return 0,0 coordinate.
+     * 
+     * @return Point of mouse coordinates.
+     */
+    private Point mousePosition()
+    {
+        try
+        {
+            Point mp = this.getMousePosition();
+            
+            if(mp != null)
+                return this.getMousePosition();
+            else
+                return new Point(0, 0);
+        }
+        catch (Exception e)
+        {
+            return new Point(0, 0);
+        }
+    }
+    
+    /**
+     * This method is called when keyboard key is released.
+     * 
+     * @param event KeyEvent
+     */
+    @Override
+    public void keyReleasedFramework(KeyEvent event)
+    {
+        switch (gs)
+        {
+            case GAMEOVER:
+                if(event.getKeyCode() == KeyEvent.VK_ESCAPE)
+                    System.exit(0);
+                else if(event.getKeyCode() == KeyEvent.VK_SPACE || event.getKeyCode() == KeyEvent.VK_ENTER)
+                    restartGame();
+            break;
+            case PLAYING:
+            case MAIN_MENU:
+                if(event.getKeyCode() == KeyEvent.VK_ESCAPE)
+                    System.exit(0);
+            break;
+        }
+    }
+    
+    /**
+     * This method is called when mouse button is clicked.
+     * 
+     * @param event MouseEvent
+     */
+    @Override
+    public void mouseClicked(MouseEvent event)
+    {
+        switch (gs)
+        {
+            case MAIN_MENU:
+                if(event.getButton() == MouseEvent.BUTTON1)
+                    newGame();
+            break;
+        }
+    }
+}
---
+++ DuckHunter/src/duckhunter/Shooter.java
@@ -0,0 +1,219 @@
+package duckhunter;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.logging.*;
+import javax.imageio.ImageIO;
+
+public class Shooter {
+	private Random random;
+	private Font font;
+	private ArrayList<Duck> ducks;
+	private int numRunAwayDucks, numKilledDucks, score, numShots;
+	private long lastTimeShoot, timeBetweenShots;
+	private BufferedImage backgroundImg, grassImg, duckImg, sightImg;
+	private int sightImgMidW, sightImgMidH;
+
+	public Shooter(){
+		Framework.gs = Framework.gameState.GAME_CONTENT_LOADING;
+
+		Thread threadForInitGame = new Thread() {
+			@Override
+			public void run(){
+				init();
+				load();
+				Framework.gs = Framework.gameState.PLAYING;
+			}
+		};
+		threadForInitGame.start();
+	}
+
+
+	/**
+	 * Set variables and objects for the game.
+	 */
+	private void init()
+	{
+		random = new Random();        
+		font = new Font("monospaced", Font.BOLD, 18);
+
+		ducks = new ArrayList<Duck>();
+
+		numRunAwayDucks = 0;
+		numKilledDucks = 0;
+		score = 0;
+		numShots = 0;
+
+		lastTimeShoot = 0;
+		timeBetweenShots = Framework.secInNanosec / 3;
+	}
+
+	/**
+	 * Load game files - images, sounds, ...
+	 */
+	private void load()
+	{
+		try
+		{
+			URL backgroundURL = this.getClass().getResource("/shoot_the_duck/resources/images/background.jpg");
+			backgroundImg = ImageIO.read(backgroundURL);
+
+			URL grassURL = this.getClass().getResource("/shoot_the_duck/resources/images/grass.png");
+			grassImg = ImageIO.read(grassURL);
+
+			URL duckImgUrl = this.getClass().getResource("/shoot_the_duck/resources/images/duck.png");
+			duckImg = ImageIO.read(duckImgUrl);
+
+			URL sightURL = this.getClass().getResource("/shoot_the_duck/resources/images/sight.png");
+			sightImg = ImageIO.read(sightURL);
+			sightImgMidW = sightImg.getWidth() / 2;
+			sightImgMidH = sightImg.getHeight() / 2;
+		}
+		catch (IOException ex) {
+			Logger.getLogger(Shooter.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+
+	/**
+	 * Restart game - reset some variables.
+	 */
+	public void restart()
+	{
+		// Removes all of the ducks from this list.
+		ducks.clear();
+
+		// We set last duckt time to zero.
+		Duck.lastDuckTime = 0;
+
+		numRunAwayDucks = 0;
+		numKilledDucks = 0;
+		score = 0;
+		numShots = 0;
+
+		lastTimeShoot = 0;
+	}
+
+
+	/**
+	 * Update game logic.
+	 * 
+	 * @param gameTime gameTime of the game.
+	 * @param mousePosition current mouse position.
+	 */
+	public void update(long gameTime, Point mousePosition)
+	{
+		// Creates a new duck, if it's the time, and add it to the array list.
+		if(System.nanoTime() - Duck.lastDuckTime >= Duck.timeBetweenDucks)
+		{
+			// Here we create new duck and add it to the array list.
+			ducks.add(new Duck(Duck.duckLines[Duck.nextDuckLines][0] + random.nextInt(200), Duck.duckLines[Duck.nextDuckLines][1], Duck.duckLines[Duck.nextDuckLines][2], Duck.duckLines[Duck.nextDuckLines][3], duckImg));
+
+			// Here we increase nextDuckLines so that next duck will be created in next line.
+			Duck.nextDuckLines++;
+			if(Duck.nextDuckLines >= Duck.duckLines.length)
+				Duck.nextDuckLines = 0;
+
+			Duck.lastDuckTime = System.nanoTime();
+		}
+
+		// Update all of the ducks.
+		for(int i = 0; i < ducks.size(); i++)
+		{
+			// Move the duck.
+			ducks.get(i).Update();
+
+			// Checks if the duck leaves the screen and remove it if it does.
+			if(ducks.get(i).x < 0 - duckImg.getWidth())
+			{
+				ducks.remove(i);
+				numRunAwayDucks++;
+			}
+		}
+
+		// Does player numShots?
+		if(Canvas.mouseButtonState(MouseEvent.BUTTON1))
+		{
+			// Checks if it can shoot again.
+			if(System.nanoTime() - lastTimeShoot >= timeBetweenShots)
+			{
+				numShots++;
+
+				// We go over all the ducks and we look if any of them was shoot.
+				for(int i = 0; i < ducks.size(); i++)
+				{
+					// We check, if the mouse was over ducks head or body, when player has shot.
+					if(new Rectangle(ducks.get(i).x + 18, ducks.get(i).y     , 27, 30).contains(mousePosition) ||
+							new Rectangle(ducks.get(i).x + 30, ducks.get(i).y + 30, 88, 25).contains(mousePosition))
+					{
+						numKilledDucks++;
+						score += ducks.get(i).score;
+
+						// Remove the duck from the array list.
+						ducks.remove(i);
+
+						// We found the duck that player shoot so we can leave the for loop.
+						break;
+					}
+				}
+
+				lastTimeShoot = System.nanoTime();
+			}
+		}
+
+		// Game ends if 10 ducks ran away
+		if(numRunAwayDucks >= 10){
+			Framework.gs = Framework.gameState.GAMEOVER;
+		}
+	}
+
+	/**
+	 * Draw the game to the screen.
+	 * 
+	 * @param g Graphics2D
+	 * @param mousePosition current mouse position.
+	 */
+	public void paint(Graphics2D g, Point mousePosition){
+		g.drawImage(backgroundImg, 0, 0, Framework.frameWidth, Framework.frameHeight, null);
+
+		// Here we draw all the ducks.
+		for(int i = 0; i < ducks.size(); i++)
+		{
+			ducks.get(i).Draw(g);
+		}
+
+		g.drawImage(grassImg, 0, Framework.frameHeight - grassImg.getHeight(), Framework.frameWidth, grassImg.getHeight(), null);
+
+		g.drawImage(sightImg, mousePosition.x - sightImgMidW, mousePosition.y - sightImgMidH, null);
+
+		g.setFont(font);
+		g.setColor(Color.darkGray);
+
+		g.drawString("RUNAWAY: " + numRunAwayDucks, 10, 21);
+		g.drawString("KILLS: " + numKilledDucks, 160, 21);
+		g.drawString("SHOOTS: " + numShots, 299, 21);
+		g.drawString("SCORE: " + score, 440, 21);
+	}
+
+
+	/**
+	 * Draw the game over screen.
+	 * 
+	 * @param g Graphics2D
+	 * @param mousePosition Current mouse position.
+	 */
+	public void gameIsOver(Graphics2D g, Point mousePosition){
+		paint(g, mousePosition);
+
+		g.setColor(Color.black);
+		g.drawString("Game Over", Framework.frameWidth / 2 - 39, (int)(Framework.frameHeight * 0.65) + 1);
+		g.drawString("Press space or enter to restart.", Framework.frameWidth / 2 - 149, (int)(Framework.frameHeight * 0.70) + 1);
+		g.setColor(Color.red);
+		g.drawString("Game Over", Framework.frameWidth / 2 - 40, (int)(Framework.frameHeight * 0.65));
+		g.drawString("Press space or enter to restart.", Framework.frameWidth / 2 - 150, (int)(Framework.frameHeight * 0.70));
+	}
+}
